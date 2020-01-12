410ddf23b153a8dab70e7ae033e96e88c1e45dc9
pazgalev <m.pazgalev@oy2b.ru>
2020-01-12 03:10:55 +0300

    Add .env
    Added to ignore .env file, create .env.example, create db.php config file and some minor updates

---
+++ app/app/db.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'dbname' => getenv('DB_NAME'),
+    'host' => getenv('DB_HOST'),
+    'user' => getenv('DB_USER'),
+    'password' => getenv('DB_PASSWORD')
+];
--- app/app/settings.php
+++ app/app/settings.php
@@ -4,16 +4,22 @@ declare(strict_types=1);
 use DI\ContainerBuilder;
 use Monolog\Logger;
 
+
 return function (ContainerBuilder $containerBuilder) {
+
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+
     // Global Settings Object
     $containerBuilder->addDefinitions([
         'settings' => [
             'displayErrorDetails' => true, // Should be set to false in production
             'logger' => [
-                'name' => 'slim-app',
+                'name' => getenv('APP_NAME'),
                 'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
                 'level' => Logger::DEBUG,
             ],
+            'db' => require_once 'db.php'
         ],
     ]);
 };
